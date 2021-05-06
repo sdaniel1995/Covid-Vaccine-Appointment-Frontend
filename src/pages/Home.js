e4c99f12e4cbbb277cf22fa11f315d1c9db1d14a
christian zuna <christian.zunalargo@revature.net>
2021-05-06 14:11:24 -0400

    added a css forlder and Distributor registration

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+// import '../css/Homepage.css';
+
 
 class Home extends Component {
   render() {
     return (
       <div className="container">
         <h1>Home Page</h1>
-        <Link to="/registration" className="btn btn-primary m-2">Register</Link>
-        <Link to="/distsignin" className="btn btn-primary m-2">Distributor Signin</Link>
-        <Link to="/patientsignin" className="btn btn-primary m-2">Patient Signin</Link>
+        <Link to="/PatientRegistration" className="btn btn-primary m-2" id="patient-registration">Patient Register</Link>
+        <Link to="/DistributorRegistration" className="btn btn-primary m-2" id="dist-registration">Distributor Registration</Link>
+        <Link to="/distsignin" className="btn btn-primary m-2" id="dist-sign">Distributor Signin</Link>
+        <Link to="/patientsignin" className="btn btn-primary m-2" id="patient-sign">Patient Signin</Link>
       </div>
     );
   }
--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -5,36 +5,27 @@ import PatientSignin from './PatientSignin';
 import PatientRegistration from './PatientRegistration';
 import Dashboard from './Dashboard';
 import DashboardConfig from './DashboardConfig';
-import Header from './Header';
 import Home from './Home';
-import Requests from './Requests';
 import DistributorSignin from './DistributorSignin'
 import DistributorDashboard from './DistributorDashboard'
 import CreateAppointment from './CreateAppointment'
 import FindAppointment from './FindAppointment'
+import DistributorRegistration from './DistributorRegistration'
 
 class Layout extends Component {
   render() {
     return (
       <div>
-        <div className="fixed-top mb-5">
-          <Header />
-        </div>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
         <div className="container-fluid mb-5 mt-5">
           <Route exact path="/" component={Home} />
           <Route path="/main" component={Home} />
-          <Route path="/registration" component={PatientRegistration} />
+          <Route path="/DistributorRegistration" component={DistributorRegistration} />
+          <Route path="/PatientRegistration" component={PatientRegistration} />
           <Route path="/distsignin" component={DistributorSignin} />
           <Route path="/patientsignin" component={PatientSignin} />
           <Route path="/portal" component={PatientProfile} />
           <Route path="/dash" component={Dashboard} />
           <Route path="/dashconfig" component={DashboardConfig} />
-          <Route path="/requests" component={Requests} />
           <Route path="/DistributorDashboard" component={DistributorDashboard} />
           <Route path="/CreateAppointment" component={CreateAppointment} />
           <Route path="/PatientProfile" component={PatientProfile} />
